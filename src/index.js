3a689ae408ffb43930cf6c1de18fa72de74af887
Jonathon Leight <jonathon.leight@jleight.com>
2017-10-10 18:32:20 -0400

    Fix quotes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export { default as ConfirmOverlayProvider } from "./components/Provider";
-export { default as confirmOverlay } from "./mixins/confirmOverlay";
+export { default as ConfirmOverlayProvider } from './components/Provider';
+export { default as confirmOverlay } from './mixins/confirmOverlay';
